66fa8a638a2eeb528426b89e927367e8f68ca8a8
januszewskijanuszek <januszewskijanuszek@gmail.com>
2023-01-13 16:09:22 +0100

    Init

---
+++ code/src/App.js
@@ -0,0 +1,12 @@
+//App.js
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+      <Routes>
+
+      </Routes>
+  );
+}
+export default App;
